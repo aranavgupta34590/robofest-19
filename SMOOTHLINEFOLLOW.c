06e6de2e7463617db507cc6f0c4e52e31e7a666c
wangyuji431 <wangyuji431@gmail.com>
2018-07-29 11:46:51 -0400

    added basic linefollow

---
+++ SMOOTHLINEFOLLOW.c
@@ -0,0 +1,17 @@
+#define WHITE 10
+#define BLACK 0
+#define CENTER 5
+task main()
+{
+
+
+ while(1)
+ {
+
+ int error = getColorReflected(S1)-CENTER;
+
+   setMotorSpeed (motorA,50 + error);
+   setMotorSpeed (motorB,50 - error);
+
+ }
+}
